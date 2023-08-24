47599e458b8e218b81d9238dec8b612dce24ec62
Manfred Stocker <manfred.stocker@jetticket.at>
2023-08-24 15:13:43 +0200

    Initial commit'

---
+++ src/MyHost.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace CSharpGracefulShutdownExample;
+internal class MyHost : IHostedService
+{
+    private bool _shouldStop;
+    private Task? _backgroundTask;
+
+    private readonly IHostApplicationLifetime _lifetime;
+
+    public MyHost(IHostApplicationLifetime applicationLifetime) => 
+        _lifetime = applicationLifetime;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+
+        Console.WriteLine("starting the service");
+
+        _backgroundTask = Task.Run(async () =>
+        {
+            while (!_shouldStop)
+            {
+                Console.WriteLine("service is running");
+                await Task.Delay(50);
+            }
+
+            Console.WriteLine("Background task gracefully stopped");
+        }, cancellationToken);
+
+        return Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("stopping the service");
+        _shouldStop = true;
+        await _backgroundTask!;
+        Console.WriteLine("Service stopped");
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using CSharpGracefulShutdownExample;
+
+var host = new HostBuilder()
+    .ConfigureServices((hostContext, services) => services.AddHostedService<MyHost>())
+    .UseConsoleLifetime()
+    .Build();
+
+await host.RunAsync();
